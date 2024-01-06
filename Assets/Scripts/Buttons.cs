6a0d0b4f7ea8a2b64f597005866bb50ba80c8a7d
NYAPY <thoth20030811@gmail.com>
2024-01-06 11:22:11 +0900

    베타버젼 v0.1

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -5,16 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class Buttons : MonoBehaviour
 {
+    public GameObject CanvasGroup, BackLight;
+    public float time = 1.0f;
+    bool SetBtnOn, BackBtnOn = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        BackLight = GameObject.FindWithTag("BackLight");
+        CanvasGroup = GameObject.FindWithTag("CanvasGroup");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (SetBtnOn)
+        {
+            CanvasGroup.transform.position = Vector3.MoveTowards(CanvasGroup.transform.position, new Vector3(0, 12, 0), (12 / time) * Time.deltaTime);
+            if (CanvasGroup.transform.position == new Vector3(0, 12, 0))
+            {
+                SetBtnOn = false;
+            }
+        }
+        if (BackBtnOn)
+        {
+            CanvasGroup.transform.position = Vector3.MoveTowards(CanvasGroup.transform.position, new Vector3(0, 0, 0), (12 / time) * Time.deltaTime);
+            if (CanvasGroup.transform.position == new Vector3(0, 0, 0))
+            {
+                BackBtnOn = false;
+            }
+        }
     }
 
     public void StartButton()
@@ -24,7 +43,7 @@ public class Buttons : MonoBehaviour
 
     public void SettingButton()
     {
-
+        SetBtnOn = true;
     }
     public void ExitButton()
     {
@@ -33,5 +52,17 @@ public class Buttons : MonoBehaviour
         #else
                 Application.Quit();
         #endif
+    }
+    public void BackButton()
+    {
+        BackBtnOn = true;
+    }
+    public void HIGHButton()
+    {
+        BackLight.GetComponent<BackLight>().HighQuality = true;
+    }
+    public void LOWButton()
+    {
+        BackLight.GetComponent<BackLight>().HighQuality = false;
     }
 }
